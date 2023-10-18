ef3053e022a10f9796f4da609e839f2e647c717a
joaovictor-ferreira <joaovictor2111@gmail.com>
2023-10-18 20:24:21 -0300

    feat: aluno refactor

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,14 +1,15 @@
 public class Aluno<T> {
-    int matricula;
-    String nome;
+    private int matricula;
+    private String nome;
 
     public Aluno(int matricula, String nome) {
         this.matricula = matricula;
         this.nome = nome;
     }
+
     public Aluno(int matricula) {
         this.matricula = matricula;
-
+        this.nome = "Nome Padrão";
     }
 
     public int getMatricula() {
